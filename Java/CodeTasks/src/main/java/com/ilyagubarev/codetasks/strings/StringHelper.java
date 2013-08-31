c0dde6f1a5c777331e93ce61a402b6fa20424c09
Ilya Gubarev <ilya.gubarev@gmail.com>
2013-08-31 13:10:53 +0400

    StringHelper added

---
+++ Java/CodeTasks/src/main/java/com/ilyagubarev/codetasks/strings/StringHelper.java
@@ -0,0 +1,15 @@
+package com.ilyagubarev.codetasks.strings;
+
+/**
+ * String processing methods.
+ *
+ * @version 1.01, 31 August 2013
+ * @since 31 August 2013
+ * @author Ilya Gubarev
+ */
+public final class StringHelper {
+
+    private StringHelper() {
+
+    }
+}
