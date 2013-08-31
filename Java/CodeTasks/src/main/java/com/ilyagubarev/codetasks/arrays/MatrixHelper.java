c110bcf97f4e05aebeb8f32dbc215b25366cf034
Ilya Gubarev <ilya.gubarev@gmail.com>
2013-08-31 13:47:14 +0400

    MatrixHelper added

---
+++ Java/CodeTasks/src/main/java/com/ilyagubarev/codetasks/arrays/MatrixHelper.java
@@ -0,0 +1,16 @@
+package com.ilyagubarev.codetasks.arrays;
+
+/**
+ * Matrix processing methods.
+ *
+ * @version 1.01, 31 August 2013
+ * @since 31 August 2013
+ * @author Ilya Gubarev
+ */public final class MatrixHelper {
+
+    
+
+    private MatrixHelper() {
+
+    }
+}
