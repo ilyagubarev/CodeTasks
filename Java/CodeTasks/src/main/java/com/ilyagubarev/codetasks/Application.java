6adce3bb46bd23127cab306e93f0159e132260a3
Ilya Gubarev <ilya.gubarev@gmail.com>
2013-08-31 13:06:54 +0400

    CodeTasks (Java) inited

---
+++ Java/CodeTasks/src/main/java/com/ilyagubarev/codetasks/Application.java
@@ -0,0 +1,24 @@
+package com.ilyagubarev.codetasks;
+
+/**
+ * Application main class
+ *
+ * @version 1.01, 31 August 2013
+ * @since 31 August 2013
+ * @author Ilya Gubarev
+ */
+public final class Application {
+
+    /**
+     * Application execution entry point.
+     *
+     * @param args command line arguments.
+     */
+    public static void main(String ... args) {
+
+    }
+
+    private Application() {
+
+    }
+}
